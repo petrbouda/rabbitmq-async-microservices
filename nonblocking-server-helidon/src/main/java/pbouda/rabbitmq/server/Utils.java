3acceac78add4ff5117e77ca2c92c4091dc27d50
petrbouda <bouda.petr@gmail.com>
2019-05-31 20:36:00 +0200

    UUID and logging generovani

--- nonblocking-server-helidon/src/main/java/pbouda/rabbitmq/server/Utils.java
+++ nonblocking-server-helidon/src/main/java/pbouda/rabbitmq/server/Utils.java
@@ -2,15 +2,25 @@ package pbouda.rabbitmq.server;
 
 import com.thedeanda.lorem.Lorem;
 import com.thedeanda.lorem.LoremIpsum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 
+import java.util.UUID;
+
 final class Utils {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Utils.class);
 
     static Message request() {
         Lorem LOREM = LoremIpsum.getInstance();
         String name = LOREM.getFirstName() + " " + LOREM.getLastName();
+
+        UUID messageID = UUID.randomUUID();
+        LOG.info("We are sending name " + name + " with ID: " + messageID.toString());
+
         return new Message(name.getBytes(), new MessageProperties());
     }
 
